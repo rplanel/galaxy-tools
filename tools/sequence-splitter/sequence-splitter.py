5904e24d21998ad941d8c5d28c9877a80fad1c12
Remi Planel <rplanel@pasteur.fr>
2019-08-08 14:08:54 +0200

    Pass to python 3, one letter options and more doc

--- tools/sequence-splitter/sequence-splitter.py
+++ tools/sequence-splitter/sequence-splitter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # coding: utf-8
 
 import locale
@@ -7,10 +7,13 @@ import os
 import argparse
 from itertools import chain, islice, tee, count
 import logging
+
 # BioPython
 from Bio import SeqIO
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s : %(levelname)s : %(message)s')
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s : %(levelname)s : %(message)s"
+)
 logger = logging.getLogger()
 
 
@@ -22,7 +25,7 @@ def main():
     _, filename = os.path.split(basename)
     chunk_size = args.chunk_size
     # split the sequences in chunks
-    if (chunk_size):
+    if chunk_size:
         logger.info('%s = %s', "chunk size asked", chunk_size)
         sequences_record = gen_sequence_record(args.sequences, args.format)
         chunks = gen_get_chunks_by_size(sequences_record, chunk_size)
@@ -37,8 +40,7 @@ def main():
 def gen_get_chunks(sequences_path, sequences_format, nb_chunk):
 
     # First record to count the sequences
-    sequences_record_to_count = gen_sequence_record(
-        sequences_path, sequences_format)
+    sequences_record_to_count = gen_sequence_record(sequences_path, sequences_format)
     # Get the number of sequences
     nb_sequences = get_nb_sequences(sequences_record_to_count)
     logger.info('%s = %i', "Number of sequences", nb_sequences)
@@ -70,32 +72,35 @@ def write_chunks(iterable, dirname, filename, file_extension, sequence_format):
         if not os.path.exists(dirname):
             os.mkdir(dirname)
         output_file = os.path.join(
-            dirname, filename + '-chunk-' + str(idx+1) + file_extension)
-        with open(output_file, mode='w') as output_handle:
+            dirname, filename + "-chunk-" + str(idx + 1) + file_extension
+        )
+        with open(output_file, mode="w") as output_handle:
             count_seq, seq_to_write = tee(chunk, 2)
-            logger.info('%s : number of seuquences = %i',
-                        output_file, len(list(count_seq)))
+            logger.info(
+                "%s : number of seuquences = %i", output_file, len(list(count_seq))
+            )
             SeqIO.write(seq_to_write, output_handle, sequence_format)
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Split fastq file")
-    parser.add_argument("-s", "--sequences", type=str,
-                        help="File that contains the sequences")
+    parser = argparse.ArgumentParser(description="Split fasta/fastq files")
+    parser.add_argument(
+        "-s", "--sequences", type=str, help="File that contains the sequences"
+    )
 
-    parser.add_argument("-f", "--format", type=str,
-                        help="File format (fastq, fasta)")
+    parser.add_argument("-f", "--format", type=str, help="File format (fastq, fasta)")
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-cs", "--chunk-size", type=int,
-                       help="Size of the chunks. Cannot be used  ")
-    group.add_argument("-nc", "--nb-chunk", help="Number of chunk", type=int)
+    group.add_argument(
+        "-c", "--chunk-size", type=int, help="The number of sequences by chunks."
+    )
+    group.add_argument("-n", "--nb-chunk", help="Number of chunks", type=int)
 
-    parser.add_argument("-o", "--output", type=str, default="./")
+    parser.add_argument("-o", "--output", type=str, default="./", help="The output directory where the chunks will be saved")
 
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logger.info("START")
     main()
     logger.info("FINISHED")
